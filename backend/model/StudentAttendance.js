5d0b67be87ee2864e2d49fb206d1fbda196f379c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-25 13:39:04 +0600

    student attendence

--- backend/model/StudentAttendance.js
+++ backend/model/StudentAttendance.js
@@ -1,7 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const studentattendenceSchema = new mongoose.Schema({
-
+     createdDate : Date,
+     user:{
+        type:Schema.Types.ObjectId,
+        ref: 'User'
+     },
+     adminAttendence:{
+        type: Schema.Types.ObjectId,
+        ref: 'AdminAttendence'
+     }
 },
 {timestamps:true}
 );
