4802b609937043bc15d71a610af6c0fa30c49292
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-25 13:28:52 +0600

    profile model

--- backend/model/Profile.js
+++ backend/model/Profile.js
@@ -1,6 +1,22 @@
-import { mongoose } from "mongoose";
+import { mongoose, Schema } from "mongoose";
 
 const profileSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required: true
+    },
+    lastName:{
+        type:String,
+        required: true
+    },
+    phone:{
+        type:String
+    },
+    avatar:String,
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+    },
 
 },
 {timestamps: true}
