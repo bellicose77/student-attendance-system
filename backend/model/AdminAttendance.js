c84b184e6770be3662c8e6b2a9324c8bb9ffdcb3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-25 13:33:23 +0600

    admin model

--- backend/model/AdminAttendance.js
+++ backend/model/AdminAttendance.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 
 const adminattendenceSchema = new mongoose.Schema({
+    timilimit:{
+        type: Number
+    },
+    status:String,
+    createdTime:{
+        type:Date
+    }
 
 },
 {
