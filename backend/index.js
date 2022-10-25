c510314f6c2306549c6e1781d7353bf8ec03404b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-25 12:30:19 +0600

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config()
@@ -7,14 +8,17 @@ const port = 8000;
 
 const connect = async()=>{
     try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Database connected")
 
     }
     catch(e){
-        
+        throw(e)
     }
 }
 
 
 app.listen(port, () => {
+    connect();
     console.log(`Example app listening on port ${port}`)
   })
