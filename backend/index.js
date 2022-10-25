768233fa20e3bf47f36cb3e77b0647fa16961b7a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-25 12:27:14 +0600

    data base connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 const port = 8000;
+
+const connect = async()=>{
+    try{
+
+    }
+    catch(e){
+        
+    }
+}
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
