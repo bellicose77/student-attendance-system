51a45a7ff4c1194b19c3eceddbbd028d4adc4d0c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-27 22:01:10 +0600

    hasing password

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,4 +1,6 @@
 import { response } from "express";
+import User from "../model/User";
+import bcrypt from 'bcrypt'
 
 export const mainHome = async(req,res)=>{
     res.json("Welcome to home page")
@@ -11,7 +13,12 @@ export const registerController = async(req,res)=>{
     if(!name || !email || !password){
         return res.status(400).json({message:"some data is missing"});
     }
-    
+    const user = await User.findOne({email:email});
+    if(user){
+        return res.status(400).json({message:"User already exits"})
+    }
+    const hash = 
+    const newUser = new User({name:name,email:email})
       
    }catch(err){
     next(err)
