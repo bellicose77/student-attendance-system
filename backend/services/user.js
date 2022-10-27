323198a28be47abb3e2c71c4a1e87fad7ef88d8a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-27 23:53:50 +0600

    register api

--- backend/services/user.js
+++ backend/services/user.js
@@ -1,5 +1,8 @@
+import User from "../model/User"
+
 export const findByProperty = (key,value)=>{
     if(key===_id){
-        
+        return User.findById(value)
     }
+    return User.findOne({[key]:value});
 }
