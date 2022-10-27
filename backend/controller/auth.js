5aabaf0b782cbe0e622452dbab7b6b39d741b1af
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-27 21:43:51 +0600

    register api

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 export const mainHome = async(req,res)=>{
     res.json("Welcome to home page")
 }
@@ -6,6 +8,9 @@ export const registerController = async(req,res)=>{
     const {name,email,password} = req.body;
     console.log(name,email)
    try{
+    if(!name || !email || !password){
+        return res.status(400).json({message:"some data is missing"});
+    }
       
    }catch(err){
     next(err)
--- backend/routes/main.js
+++ backend/routes/main.js
@@ -2,6 +2,6 @@ import authRoutes from './auth.js'
 
 import express from 'express'
 const router = express.Router();
-router.use('/api/auth',authRoutes);
+router.use('/api/v1/auth',authRoutes);
 
 export default router;
