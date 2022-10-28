c19644c2196b2f44af1afc58315789a228da01f3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-28 21:39:40 +0600

    error handle

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -1,12 +1,13 @@
 import { createNewUser, findUserByProperty } from "./user.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken';
+import error from '../utils/error.js'
 
  export const registerService = async({name,email,password})=>{
     let user = await findUserByProperty('email',email) 
 
     if(user){
-       throw error("User already exits")
+       throw error("User already exits",400)
     }
     const saltRound =10;
     const salt = bcrypt.genSaltSync(saltRound);
