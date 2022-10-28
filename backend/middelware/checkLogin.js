981d85729f7d632f470b1ad14b6d182587042434
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-28 13:53:42 +0600

    jwt verify done

--- backend/middelware/checkLogin.js
+++ backend/middelware/checkLogin.js
@@ -1,9 +1,13 @@
+import jwt from "jsonwebtoken";
+
 export const checkLogin = async (req,res,next)=>{
     const {authorization} = req.headers;
    // console.log(authorization)
     try{
-       const token = authorization.split(' ')[1]
-        console.log(token);
+       const token = authorization.split(' ')[1];
+       const decode = await jwt.verify(token,process.env.SECRET_KEY);
+        console.log("decode" ,decode);
+        const {name,email,roles}=decode;
         next();
     }catch(err){
         next("Authentication failed")
