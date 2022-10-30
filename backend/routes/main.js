887fdbca4ef28f532b1ce6238cc5ff620db301f2
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-30 23:20:16 +0600

    user route

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,7 +1,9 @@
 import authRoutes from './auth.js'
+import userRoutes from './user.js'
 
 import express from 'express'
 const router = express.Router();
 router.use('/api/v1/auth',authRoutes);
+router.use('/api/v1/users',userRoutes);
 
 export default router;
