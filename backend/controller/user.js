a7a0e406cda89bff092ba949236b3cd31b4c1a4a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 00:19:47 +0600

    get a single user API

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,4 +1,4 @@
-import { findUsers } from "../services/user.js"
+import { findUserByProperty, findUsers } from "../services/user.js"
 import { error } from "../utils/error.js";
 
 export const getUserController = async(req,res,next)=>{
@@ -17,7 +17,13 @@ export const getUserController = async(req,res,next)=>{
 };
 
 export const getSingleUserController = async(req,res,next)=>{
+
+    
     try{
+        const {userId} = req.params;
+        const user = await findUserByProperty(userId);
+         console.log(userId);
+         return res.json({message:"here comes the request",userId})
 
     }catch(e){
         next(e);
