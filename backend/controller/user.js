ae733fa46a786235222d8bb697d05530e07cfba1
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-30 23:36:41 +0600

    get all user from database

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,5 +1,6 @@
 export const getUserController = async(req,res,next)=>{
     try{
+        return res.json({meassage:"Something is coming"})
 
     }catch(e){
         next(e)
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { getUserController } from '../controller/user';
+import { getUserController } from '../controller/user.js';
 
 const router = express.Router();
 //get single user
-router.get('/:userId')
+router.get('/:userId');
+//delete a single user 
+router.delete('/:userId');
+//create a new user
+router.post('/');
 //get all users
 router.get('/',getUserController);
 
