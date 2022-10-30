01aa2120da36485a2adee2055a35b494b4ed855f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-30 23:31:37 +0600

    get a single user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,9 @@ import express from 'express'
 import { getUserController } from '../controller/user';
 
 const router = express.Router();
-
+//get single user
+router.get('/:userId')
+//get all users
 router.get('/',getUserController);
 
 export default router;
