69ac2afa86deae0d485bc5e94c67635827dd3ac1
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 00:05:56 +0600

    adding authoraization

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -2,8 +2,9 @@ import authRoutes from './auth.js'
 import userRoutes from './user.js'
 
 import express from 'express'
+import { checkLogin } from '../middelware/checkLogin.js';
 const router = express.Router();
 router.use('/api/v1/auth',authRoutes);
-router.use('/api/v1/users',userRoutes);
+router.use('/api/v1/users', checkLogin, userRoutes);
 
 export default router;
