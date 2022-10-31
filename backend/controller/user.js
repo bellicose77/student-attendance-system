5112bee5f6121af252fd429f7356847da7787b99
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 14:36:50 +0600

    update api

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -50,7 +50,7 @@ export const deleteById = async(req,res,next)=>{
 
 export const updateById = async(req,res,next)=>{
     const {userId} = req.params;
-    const {} = req.body;
+    const {name,roles,accountstatus} = req.body;
     try{
         const user = await findUserByProperty('_id',userId);
 
